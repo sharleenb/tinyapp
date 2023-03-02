80e23cf8921191265768d0659854eb70b7e64bc8
Sharleen Beniamin <sharleenb16@gmail.com>
2023-03-02 20:10:35 +0000

    include header partial

--- express_server.js
+++ express_server.js
@@ -19,7 +19,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 

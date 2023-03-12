74977fefba66b893861f6421744cd3a2f10b55e9
Sharleen Beniamin <sharleenb16@gmail.com>
2023-03-12 20:24:28 +0000

    store database objects

---
+++ database.js
@@ -0,0 +1,29 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  b2XVn2: {
+    longURL: "https://www.lighthouselabs.ca",
+    userID: "bJ48lX",
+  },
+};
+
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "abc",
+  },
+  bJ48lX: {
+    id: "bJ48lX",
+    email: "user2@example.com",
+    password: "123",
+  },
+};
+
+module.exports = {urlDatabase, users};
